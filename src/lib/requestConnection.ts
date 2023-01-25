e628a59224c49a415b3478e469773e47673592fd
Clement Seconde-Kynnersely <c.sec.kyn@gmail.com>
2023-01-25 20:24:12 +0000

    merging histories

--- src/lib/requestConnection.ts
+++ src/lib/requestConnection.ts
@@ -1,8 +1,8 @@
-import { connectToDB } from './db';
+// import { connectToDB } from './db';
 
-export const handle = async ({ event, resolve }) => {
-  const dbconn = connectToDB();
-  event.locals = { dbconn };
-  const response = await resolve(event);
-  dbconn.release();
-};
+// export const handle = async ({ event, resolve }) => {
+//   const dbconn = connectToDB();
+//   event.locals = { dbconn };
+//   const response = await resolve(event);
+//   dbconn.release();
+// };
