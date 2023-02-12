7b8f3ec508918e60cf79fa7eca42567c6b5346e9
Clement Seconde-Kynnersely <c.sec.kyn@gmail.com>
2023-02-12 23:35:24 +0000

    email auth + prisma db commection WIP

--- src/routes/api/auth/[...solidAuth].ts
+++ src/routes/api/auth/[...solidAuth].ts
@@ -2,14 +2,36 @@
 import { SolidAuth, SolidAuthConfig } from '@auth/solid-start';
 // import { SolidAuth, type SolidAuthConfig } from "@auth/solid-start"
 import GitHub from '@auth/core/providers/github';
+import EmailProvider from '@auth/core/providers/email';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
 
-export const authOpts: any = {
+const prisma = new PrismaClient();
+
+export const authOpts: SolidAuthConfig = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GitHub({
       // @ts-ignore
       clientId: import.meta.env.VITE_GITHUB_ID,
       // @ts-ignore
       clientSecret: import.meta.env.VITE_GITHUB_SECRET,
+    }),
+    EmailProvider({
+      server: {
+        // @ts-ignore
+        host: import.meta.env.EMAIL_SERVER_HOST,
+        // @ts-ignore
+        port: import.meta.env.EMAIL_SERVER_PORT,
+        auth: {
+          // @ts-ignore
+          user: import.meta.env.EMAIL_SERVER_USER,
+          // @ts-ignore
+          pass: import.meta.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      // @ts-ignore
+      from: import.meta.env.EMAIL_FROM,
     }),
   ],
   debug: false,
